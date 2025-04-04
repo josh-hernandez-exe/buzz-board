ad3c736d743bec53adf91ddb33622bf31b6ad7de
Josh Hernandez <i.am.mr.josh.hernandez@gmail.com>
2025-04-03 23:21:35 -0500

    feature(client): team members are listed in super admin page

--- packages/client/src/components/GameTeamSummaryCard/index.tsx
+++ packages/client/src/components/GameTeamSummaryCard/index.tsx
@@ -1,6 +1,6 @@
 import { GenericCard } from "@/components/GenericCard";
 
-import type { GameTeam } from "@/types/serverTypes";
+import type { GameTeam, GameUser } from "@/types/serverTypes";
 import { SingleBuzzerState } from "@/types/serverTypes";
 import { trpc } from "@/utils/trpc";
 
@@ -18,10 +18,12 @@ export function GameTeamSummaryCard({
   const gameTeamQuery = trpc.gameTeam.byId.useQuery(gameTeamId);
   const buzzerQuery = trpc.buzzerState.findByGameId.useQuery(gameId);
   const scoreboardQuery = trpc.scoreboard.findByGameId.useQuery(gameId);
+  const gameUsersQuery = trpc.gameUser.findByGameTeamId.useQuery(gameTeamId);
 
   if (gameTeamQuery.data === undefined) return undefined;
   if (buzzerQuery.data === undefined) return undefined;
   if (scoreboardQuery.data === undefined) return undefined;
+  if (gameUsersQuery.data === undefined) return undefined;
 
   const buzzerState =
     SingleBuzzerState[buzzerQuery.data[0].buzzers[gameTeamId]];
@@ -30,6 +32,12 @@ export function GameTeamSummaryCard({
     <div>
       <p>Buzzer: {buzzerState}</p>
       <p>Score: {scoreboardQuery.data[0].current[gameTeamId]}</p>
+      <p>
+        Members:
+        {gameUsersQuery.data
+          .map((gameUser: GameUser) => gameUser.name)
+          .join("; ")}
+      </p>
     </div>
   );
 
