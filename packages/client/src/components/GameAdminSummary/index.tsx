8384fc50c5cf14a96995c0a790e3bdc761b655d8
Josh Hernandez <i.am.mr.josh.hernandez@gmail.com>
2025-04-03 23:37:09 -0500

    feature(client): added game admin to super admin page

--- packages/client/src/components/GameAdminSummary/index.tsx
+++ packages/client/src/components/GameAdminSummary/index.tsx
@@ -7,11 +7,16 @@ export function GameAdminSummary({ gameId }: { gameId: Game["id"] }) {
   logger.debug(`GameAdminSummary: $${gameId}`);
 
   const gameQuery = trpc.game.byId.useQuery(gameId);
+  const gameAdminQuery = trpc.gameAdmin.findByGameId.useQuery(gameId);
+
+  if (gameQuery.data === undefined) return undefined;
+  if (gameAdminQuery.data === undefined) return undefined;
 
   return (
     <div>
       <p>Game ID: {gameId}</p>
-      <p>Game Name: {gameQuery.data?.name}</p>
+      <p>Game Name: {gameQuery.data.name}</p>
+      <p>Game Admin Name: {gameAdminQuery.data[0].name}</p>
     </div>
   );
 }
