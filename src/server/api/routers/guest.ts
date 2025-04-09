1fc81b0dd1d21fc7c66f2c0c02d9d2371f42bade
Josh Hernandez <i.am.mr.josh.hernandez@gmail.com>
2025-04-09 08:11:11 -0500

    feature(server): added guest router
    - currently a guest authed user can join a game
    - no UI uses it at the moment.

---
+++ src/server/api/routers/guest.ts
@@ -0,0 +1,39 @@
+import { GameFormat } from "@prisma/client";
+import { z } from "zod";
+
+import { logger } from "@/utils/logger";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+import { generateShortCode } from "@/utils/codeGeneration";
+
+export const user = createTRPCRouter({
+  game: {
+    joinAsGuest: publicProcedure
+      .input(z.object({ gameCode: z.string(), token: z.optional(z.string()) }))
+      .mutation(async ({ ctx, input }) => {
+        const { guestUser } = ctx.guestSession;
+
+        const game = await ctx.db.game.findUnique({
+          where: { code: input.gameCode },
+        });
+
+        if (!game) {
+          throw new Error("Game not found");
+        }
+
+        const curNumPlayers = await ctx.db.gameUser.count({
+          where: { gameId: game.id },
+        });
+
+        const guestGameUser = await ctx.db.gameUser.create({
+          data: {
+            gameId: game.id,
+            name: `Player ${curNumPlayers + 1}`,
+          },
+        });
+
+        return { gameUser: guestGameUser };
+      }),
+  },
+});
