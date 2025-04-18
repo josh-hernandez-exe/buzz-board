a82752483719ae54a6772d27f1fba83ef946716e
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-04-18 05:06:09 +0000

    feat(app): root page redirects to dashboard when logged in. and dashboard takes you to root page when logged out.
    - also added logout link in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { type Game } from "@prisma/client";
 
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import { api, HydrateClient } from "@/trpc/server";
 
 import { auth } from "@/server/auth";
@@ -13,6 +15,10 @@ export default async function DashboardPage() {
   const session = await auth();
   const games = await api.user.game.getAll();
 
+  if (!session) {
+    redirect("/dashboard");
+  }
+
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -20,6 +26,12 @@ export default async function DashboardPage() {
           <p className="text-center text-2xl text-white">
             {session && <span>Logged in as {session.user?.name}</span>}
           </p>
+          <Link
+            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          >
+            {session ? "Sign out" : "Sign in"}
+          </Link>
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create New Game
           </h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
 import { auth } from "@/server/auth";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   const session = await auth();
+
+  if (session) {
+    redirect("/dashboard");
+  }
 
   return (
     <HydrateClient>
@@ -13,9 +19,6 @@ export default async function Home() {
             Buzz-Board
           </h1>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
             <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
               className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
