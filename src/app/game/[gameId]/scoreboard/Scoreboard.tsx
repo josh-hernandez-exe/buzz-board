fea3ca09350b8f39fa84645ba3f3f15d0c2c4143
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-04-20 04:26:53 +0000

    feat(app): created a private scoreboard page
    - team tiles have a score and change color based of buzzer state

---
+++ src/app/game/[gameId]/scoreboard/Scoreboard.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { api } from "@/trpc/react";
+import type { PrivateGameState, GameTeamFromState } from "@/types";
+
+import { logger } from "@/utils/logger";
+
+import { GameScoreboardTeamCard } from "@/app/_components/client/GameScoreboardTeamCard";
+import { GameBasicInfoCard } from "@/app/_components/client/GameBasicInfoCard";
+import { GameWhoBuzzedIn } from "@/app/_components/client/GameWhoBuzzedIn";
+
+export function Scoreboard({
+  gameState: initialGameState,
+}: {
+  gameState: PrivateGameState;
+}) {
+  const gameStateSub = api.gameGeneral.gameState.useSubscription();
+
+  const currentGameState = gameStateSub.data ?? initialGameState;
+
+  const { game, gameTeams } = currentGameState;
+
+  return (
+    <div>
+      <h1 className="mb-4 text-2xl font-bold">Welcome to Game {game?.name}</h1>
+      <GameWhoBuzzedIn />
+      <GameBasicInfoCard game={currentGameState.game} />
+      {gameTeams.map((gameTeam) => {
+        return (
+          <GameScoreboardTeamCard
+            key={gameTeam.id}
+            gameTeam={gameTeam as GameTeamFromState}
+          />
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/app/game/[gameId]/scoreboard/page.tsx
@@ -0,0 +1,64 @@
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { api, HydrateClient } from "@/trpc/server";
+
+import { auth, gameAuth } from "@/server/auth";
+import { Scoreboard } from "./Scoreboard";
+import { logger } from "@/utils/logger";
+
+export default async function GamePage({
+  params,
+}: {
+  params: { gameId: string };
+}) {
+  const { gameId } = await params;
+  const session = await auth();
+  const gameSession = await gameAuth({
+    headers: await headers(),
+    user: session?.user,
+  });
+
+  if (!gameId) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <h1 className="mb-4 text-2xl font-bold">Game Not Found</h1>
+          <p>No game identifier was given.</p>
+        </div>
+      </main>
+    );
+  }
+  const isGameAdminAuthed = gameSession?.gameAdmin?.gameId === gameId;
+  const isGameUserAuthed = gameSession?.gameUser?.gameId === gameId;
+
+  if (!isGameAdminAuthed && !isGameUserAuthed) {
+    logger.error("Game admin or user not found for this game.");
+    // Redirect to public scoreboard if not authenticated
+    redirect(`/game/${gameId}/scoreboard/public`);
+  }
+
+  const currentGameState = await api.gameGeneral.currentGameState();
+
+  if (!currentGameState) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <h1 className="mb-4 text-2xl font-bold">Game Not Found</h1>
+          <p>No game was found or not ready yet to be viewed.</p>
+        </div>
+      </main>
+    );
+  }
+
+  logger.info(`Game Page: ${JSON.stringify(currentGameState, null, 2)}`);
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <Scoreboard gameState={currentGameState} />
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
