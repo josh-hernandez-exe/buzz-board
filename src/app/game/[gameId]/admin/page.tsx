d2a189222423cfa6b339308e09233cc5dc0eeed7
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-04-20 15:55:18 +0000

    feat: create a link from the admin page to the scoreboard

--- src/app/game/[gameId]/admin/page.tsx
+++ src/app/game/[gameId]/admin/page.tsx
@@ -43,9 +43,12 @@ export default async function GameAdminPage({
         </div>
 
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Inspect Existing Games
-          </h1>
+          <Link
+            href={`/game/${gameId}/scoreboard`}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+          >
+            Scoreboard
+          </Link>
           <GameAdminInfo gameState={currentGameState} />
         </div>
       </main>
