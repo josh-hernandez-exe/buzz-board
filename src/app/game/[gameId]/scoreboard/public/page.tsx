9c5e14e3226ce299ea29655f9504454134a163ab
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-04-27 03:12:14 +0000

    chore(app): removed top level logger on scoreboard pages (private and public)

--- src/app/game/[gameId]/scoreboard/public/page.tsx
+++ src/app/game/[gameId]/scoreboard/public/page.tsx
@@ -35,8 +35,6 @@ export default async function GamePage({
     );
   }
 
-  logger.info(`Game Page: ${JSON.stringify(currentGameState, null, 2)}`);
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
