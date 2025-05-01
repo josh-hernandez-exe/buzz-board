f913d07d2d6a9862aee0dc52d3856fa11a43a77d
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-05-01 02:38:44 +0000

    feat(app): added a join game button on the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,12 @@ export default async function Home() {
           </h1>
           <div className="flex flex-col items-center gap-2">
             <Link
+              href="/join"
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              Join a Game
+            </Link>
+            <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
               className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
             >
