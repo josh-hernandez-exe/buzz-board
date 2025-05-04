876a92a6531f6f15ee148dc07447702a51c39630
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-05-04 05:28:32 +0000

    feat: private scoreboard splits game into info to columns

--- src/app/_components/client/GameBasicInfoCard/index.tsx
+++ src/app/_components/client/GameBasicInfoCard/index.tsx
@@ -52,26 +52,28 @@ export function GameBasicInfoCard({
         <CardTitle>Game Info</CardTitle>
         <CardDescription>Game related information</CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>Game Name: {game?.name}</p>
-        <p>Game Id: {game?.id}</p>
-        <p>Game Code: {game?.code}</p>
-        <p>Game Format: {game?.format}</p>
-        <p>
-          Game Buzzer State :{" "}
-          {game.isBuzzerListening ? "Listening" : "Not Listening"}
-        </p>
-      </CardContent>
-      <CardFooter>
-        <div className="flex flex-col items-center">
-          <p className="text-center text-sm">
-            Scan the QR code to join the game
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <CardContent className="col-span-1">
+          <p>Game Name: {game?.name}</p>
+          <p>Game Id: {game?.id}</p>
+          <p>Game Code: {game?.code}</p>
+          <p>Game Format: {game?.format}</p>
+          <p>
+            Game Buzzer State :{" "}
+            {game.isBuzzerListening ? "Listening" : "Not Listening"}
           </p>
-          <div className="flex items-center justify-center">
-            <canvas className="h-32 w-32" ref={canvasRef} />
+        </CardContent>
+        <CardFooter className="col-span-1">
+          <div className="flex flex-col items-center">
+            <p className="text-center text-sm">
+              Scan the QR code to join the game
+            </p>
+            <div className="flex items-center justify-center">
+              <canvas className="h-32 w-32" ref={canvasRef} />
+            </div>
           </div>
-        </div>
-      </CardFooter>
+        </CardFooter>
+      </div>
     </Card>
   );
 }
