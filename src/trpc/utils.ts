90cb214e7ba8c792fd7bff2d7cf183732feeab30
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2025-06-22 00:04:29 +0000

    feat(k6): add k6 stress testing suite and related configurations

--- src/trpc/utils.ts
+++ src/trpc/utils.ts
@@ -1,5 +1,8 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (typeof __ENV !== "undefined" && __ENV.BASE_URL) return __ENV.BASE_URL;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
